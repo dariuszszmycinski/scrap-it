547fa69507c0d7610f11a0f43d8359a3e4060d35
dariuszszmycinski <darekszmycinski@wp.pl>
2021-04-23 10:38:39 +0200

    Repository and controller basic classes created

--- src/main/java/dasz/controller/FileInfoController.java
+++ src/main/java/dasz/controller/FileInfoController.java
@@ -1,4 +1,26 @@
 package dasz.controller;
 
+import dasz.model.FileInfo;
+import dasz.repository.FileInfoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/files")
 public class FileInfoController {
+    private FileInfoRepository fileRepo;
+
+    public FileInfoController(FileInfoRepository fileRepo) {
+        this.fileRepo = fileRepo;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<FileInfo> getFiles(){
+        List<FileInfo> files = fileRepo.findAll();
+        return files;
+    }
 }
--- src/main/java/dasz/repository/FileInfoRepository.java
+++ src/main/java/dasz/repository/FileInfoRepository.java
@@ -1,4 +1,9 @@
 package dasz.repository;
 
-public class FileInfoRepository {
+import dasz.model.FileInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
 }

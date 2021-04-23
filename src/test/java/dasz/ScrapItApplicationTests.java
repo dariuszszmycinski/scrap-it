ec78088496c66ac077ca42016d3c2a533b9cda54
dariuszszmycinski <darekszmycinski@wp.pl>
2021-04-23 09:24:18 +0200

    Creating new Spring Boot project with: -Spring Web -Rest Repositories -MySQL Driver -Spring Data JPA -Spring Security -Validation

---
+++ src/test/java/dasz/ScrapItApplicationTests.java
@@ -0,0 +1,13 @@
+package dasz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
